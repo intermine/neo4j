1d28d4f5896f7687a1753355f8d1af8558b4a5fc
Sam Hokin <shokin@ncgr.org>
2017-05-26 15:11:48 -0600

    Finished support for ignored classes, attributes, references and collections, all denoted with neo4j-ignore="true" in there respective tags. Main method shows an example of usage and displays the ignored items with an X. Still to do: denoting node->edge conversions.

--- src/org/intermine/neo4j/Neo4jModelParser.java
+++ src/org/intermine/neo4j/Neo4jModelParser.java
@@ -27,51 +27,54 @@ import org.intermine.metadata.ReferenceDescriptor;
 import org.intermine.metadata.SAXParser;
 
 /**
- * Parse a Neo4j-modified InterMine metadata XML file to provide methods pertaining to loading data into a Neo4j graph.
+ * Parse a Neo4j-modified InterMine metadata XML file to provide instructions for loading data into a Neo4j graph.
  *
  * @author Sam Hokin
  */
 public class Neo4jModelParser {
 
-    // core IM stuff
-    Model model;
-    String modelName;
-    String packageName;
-    int version;
-
-    // Neo4j stuff
-    Set<String> ignoredClasses;
-
+    // classes, attributes, references and collections to ignore during Neo4j loading
+    Set<String> ignoredClasses;     // e.g. "Sequence"
+    Set<String> ignoredAttributes;  // e.g. "BioEntity.chadoFeatureId"
+    Set<String> ignoredReferences;  // e.g. "SequenceFeature.chromosomeLocation"
+    Set<String> ignoredCollections; // e.g. "GOAnnotation.transcripts"
+    
     /**
      * Constructor
      */
     public Neo4jModelParser() {
+        // create our sets
         ignoredClasses = new LinkedHashSet<String>();
+        ignoredAttributes = new LinkedHashSet<String>();
+        ignoredReferences = new LinkedHashSet<String>();
+        ignoredCollections = new LinkedHashSet<String>();
+        // initialization
         ignoredClasses.add("InterMineObject"); // doesn't appear in XML files but most certainly should be ignored!
     }
 
     /**
-     * Simple main method that spits out Neo4j information extracted from a model XML file on the command line
+     * Simple main method that spits out Neo4j information extracted from a model XML file on the command line.
+     * (Also serves as an example of how to use this class.)
      */
     public static void main(String[] args) throws FileNotFoundException, IOException, SAXException, ParserConfigurationException, ModelParserException {
+
+        // validation
         if (args.length!=1) {
             System.out.println("Usage: Neo4jModelParser <XML file>");
             System.exit(0);
         }
         String dataModelFilename = args[0];
 
-        // process the XML file
+        // process the XML file for Neo4j instructions
         Neo4jModelParser nmp = new Neo4jModelParser();
-        nmp.process(dataModelFilename);
+        nmp.process(new InputStreamReader(new FileInputStream(dataModelFilename)));
 
-        // output
-        System.out.println("file\t\t"+dataModelFilename);
-        System.out.println("modelName\t"+nmp.modelName);
-        System.out.println("packageName\t"+nmp.packageName);
-        System.out.println("version\t\t"+nmp.version);
-        System.out.println("--------------------------------");
-        for (ClassDescriptor classDescriptor : nmp.model.getClassDescriptors()) {
-            boolean ignored = nmp.ignoredClasses.contains(classDescriptor.getSimpleName());
+        // get the plain IM model
+        Model model = new InterMineModelParser().process(new InputStreamReader(new FileInputStream(dataModelFilename)));
+
+        // output the model with Neo4j notations
+        for (ClassDescriptor classDescriptor : model.getClassDescriptors()) {
+            boolean ignored = nmp.isIgnored(classDescriptor);
             if (ignored) {
                 System.out.print("X ");
             } else {
@@ -83,20 +86,19 @@ public class Neo4jModelParser {
                 // show attributes
                 Set<AttributeDescriptor> attrDescriptors = classDescriptor.getAttributeDescriptors();
                 for (AttributeDescriptor ad : attrDescriptors) {
+                    if (nmp.isIgnored(ad)) System.out.print("X");
                     System.out.println("\ta "+ad.getName());
                 }
                 // show references
                 Set<ReferenceDescriptor> refDescriptors = classDescriptor.getReferenceDescriptors();
                 for (ReferenceDescriptor rd : refDescriptors) {
-                    String refClass = rd.getReferencedClassDescriptor().getSimpleName();
-                    if (nmp.ignoredClasses.contains(refClass)) System.out.print("X");
+                    if (nmp.isIgnored(rd)) System.out.print("X");
                     System.out.println("\tr "+rd.getName());
                 }
                 // show collections
                 Set<CollectionDescriptor> collDescriptors = classDescriptor.getCollectionDescriptors();
                 for (CollectionDescriptor cd : collDescriptors) {
-                    String collClass = cd.getReferencedClassDescriptor().getSimpleName();
-                    if (nmp.ignoredClasses.contains(collClass)) System.out.print("X");
+                    if (nmp.isIgnored(cd)) System.out.print("X");
                     System.out.println("\tc "+cd.getName());
                 }
             }
@@ -105,48 +107,95 @@ public class Neo4jModelParser {
     }
 
     /**
-     * Read source model information in InterMine-Neo4j XML format and populate various sets and maps with Neo4j loading properties.
-     * Also returns the IM model, just as InterMineModelParser does.
+     * Return true if the provided class is to be ignored for Neo4j node loading
      *
-     * @param dataModelFilename the source model XML file to parse
-     * @return an InterMine Model
-     * @throws ModelParserException if something goes wrong with parsing the class descriptors.
+     * @param cd the ClassDescriptor
+     * @return true if the class is to be ignored in Neo4j node loading
      */
-    public Model process(String dataModelFilename) throws IOException, SAXException, ParserConfigurationException, ModelParserException {
-        // we'll want the IM model for reference
-        InterMineModelParser immp = new InterMineModelParser();
-        model = immp.process(new InputStreamReader(new FileInputStream(dataModelFilename)));
-        // now parse the file again, loading Neo4j relevant stuff
-        ModelHandler handler = new ModelHandler();
-        SAXParser.parse(new InputSource(new InputStreamReader(new FileInputStream(dataModelFilename))), handler);
-        // return the IM model just as usual
-        return model;
+    public boolean isIgnored(ClassDescriptor cd) {
+        return ignoredClasses.contains(cd.getSimpleName());
     }
 
     /**
-     * Read source model information in InterMine XML format and create a set of ClassDescriptors.
+     * Return true if the provided attribute is to be ignored for Neo4j node loading
      *
-     * @param reader the source model to parse
-     * @param packageName the package name that all the classes should be in
-     * @return a set of ClassDescriptors
+     * @param ad the AttributeDescriptor
+     * @return true if the attribute is to be ignored in Neo4j node loading
+     */
+    public boolean isIgnored(AttributeDescriptor ad) {
+        String adName = ad.getClassDescriptor().getSimpleName()+"."+ad.getName();
+        return ignoredAttributes.contains(adName);
+    }
+
+    /**
+     * Return true if the provided reference is to be ignored for Neo4j node loading
+     *
+     * @param rd the ReferenceDescriptor
+     * @return true if the reference is to be ignored in Neo4j node loading
+     */
+    public boolean isIgnored(ReferenceDescriptor rd) {
+        String rdName = rd.getClassDescriptor().getSimpleName()+"."+rd.getName();
+        return ignoredReferences.contains(rdName);
+    }
+
+    /**
+     * Return true if the provided collection is to be ignored for Neo4j node loading
+     *
+     * @param cd the CollectionDescriptor
+     * @return true if the collection is to be ignored in Neo4j node loading
+     */
+    public boolean isIgnored(CollectionDescriptor cd) {
+        String cdName = cd.getClassDescriptor().getSimpleName()+"."+cd.getName();
+        return ignoredCollections.contains(cdName);
+    }
+
+    /**
+     * Read source model information in InterMine-Neo4j XML format and populate various sets and maps with Neo4j loading properties.
+     * Also returns the IM model, just as InterMineModelParser does.
+     *
+     * @param reader a file reader providing the XML file
+     * @return an InterMine Model
      * @throws ModelParserException if something goes wrong with parsing the class descriptors.
      */
-    // @Override
-    // public Set<ClassDescriptor> generateClassDescriptors(Reader reader, String packageName) throws ModelParserException {
-    //     try {
-    //         ModelHandler handler = new ModelHandler();
-    //         handler.packageName = packageName;
-    //         SAXParser.parse(new InputSource(reader), handler);
-    //         return handler.classes;
-    //     } catch (Exception e) {
-    //         throw new ModelParserException(e);
-    //     }
-    // }
+            public void process(Reader reader) throws IOException, SAXException, ParserConfigurationException, ModelParserException {
+            // parse the file for Neo4j relevant stuff
+            SAXParser.parse(new InputSource(reader), new ModelHandler());
+    }
+
+    /**
+     * Just a getter.
+     */
+    public Set<String> getIgnoredClasses() {
+        return ignoredClasses;
+    }
+
+    /**
+     * Just a getter.
+     */
+    public Set<String> getIgnoredAttributes() {
+        return ignoredAttributes;
+    }
+    
+    /**
+     * Just a getter.
+     */
+    public Set<String> getIgnoredReferences() {
+        return ignoredReferences;
+    }
+    
+    /**
+     * Just a getter.
+     */
+    public Set<String> getIgnoredCollections() {
+        return ignoredCollections;
+    }
 
     /**
      * Extension of DefaultHandler to handle IM-Neo4j metadata file
      */
     class ModelHandler extends DefaultHandler {
+
+        // holds the current class
         SkeletonClass cls;
 
         /**
@@ -154,89 +203,45 @@ public class Neo4jModelParser {
          */
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attrs) {
-            
-            if ("model".equals(qName)) {
-                modelName = attrs.getValue("name");
-                packageName = attrs.getValue("package");
-                String versionString = attrs.getValue("version");
-                if (versionString != null) {
-                    version = Integer.parseInt(versionString);
-                }
-                
-            } else if ("class".equals(qName)) {
+
+            if ("class".equals(qName)) {
+
                 String name = attrs.getValue("name");
                 String supers = attrs.getValue("extends");
-                boolean isInterface = Boolean.valueOf(attrs.getValue("is-interface")).booleanValue();
+                cls = new SkeletonClass(name, supers);
                 boolean neo4jIgnore = Boolean.valueOf(attrs.getValue("neo4j-ignore")).booleanValue();
-                cls = new SkeletonClass(packageName, name, supers, isInterface, neo4jIgnore);
-                
+                if (neo4jIgnore) ignoredClasses.add(name);
+
             } else if ("attribute".equals(qName)) {
+
                 String name = attrs.getValue("name");
-                String type = attrs.getValue("type");
                 if (StringUtils.isEmpty(name)) {
                     throw new IllegalArgumentException("Error - `" + cls.name + "` has an attribute" + " with an empty/null name");
                 }
-                if (attrs.getType("type") == null) {
-                    throw new IllegalArgumentException("Error - type of attribute `" + name + "` not defined for `" + cls.name + "`");
-                }
-                cls.attributes.add(new AttributeDescriptor(name, type));
+                boolean neo4jIgnore = Boolean.valueOf(attrs.getValue("neo4j-ignore")).booleanValue();
+                if (neo4jIgnore) ignoredAttributes.add(cls.name+"."+name);
                 
             } else if ("reference".equals(qName)) {
+
                 String name = attrs.getValue("name");
-                String origType = attrs.getValue("referenced-type");
-                String type = origType;
                 if (StringUtils.isEmpty(name)) {
                     throw new IllegalArgumentException("Error - `" + cls.name + "` has a reference" + " with an empty/null name");
                 }
-                if (type == null) {
-                    throw new IllegalArgumentException("Error - type of reference `" + name + "` not defined for `" + cls.name + "`");
-                }
-                if (type.startsWith(packageName + ".")) {
-                    type = type.substring(packageName.length() + 1);
-                }
-                if (type.contains(".")) {
-                    throw new IllegalArgumentException("Class " + origType + " in reference " + cls.name + "." + name + " is not in the model package " + packageName);
-                }
-                if (!"".equals(packageName)) {
-                    type = packageName + "." + type;
-                }
-                String reverseReference = attrs.getValue("reverse-reference");
-                cls.references.add(new ReferenceDescriptor(name, type, reverseReference));
+                boolean neo4jIgnore = Boolean.valueOf(attrs.getValue("neo4j-ignore")).booleanValue();
+                if (neo4jIgnore) ignoredReferences.add(cls.name+"."+name);
                 
             } else if ("collection".equals(qName)) {
+
                 String name = attrs.getValue("name");
-                String origType = attrs.getValue("referenced-type");
-                String type = origType;
                 if (StringUtils.isEmpty(name)) {
                     throw new IllegalArgumentException("Error - `" + cls.name + "` has a collection" + " with an empty/null name");
                 }
-                if (type == null) {
-                    throw new IllegalArgumentException("Error - `" + name + "` collection missing a type for `" + cls.name + "`");
-                }
-                if (type.startsWith(packageName + ".")) {
-                    type = type.substring(packageName.length() + 1);
-                }
-                if (type.contains(".")) {
-                    throw new IllegalArgumentException("Class " + origType + " in reference " + cls.name + "." + name + " is not in the model package " + packageName);
-                }
-                if (!"".equals(packageName)) {
-                    type = packageName + "." + type;
-                }
-                String reverseReference = attrs.getValue("reverse-reference");
-                cls.collections.add(new CollectionDescriptor(name, type, reverseReference));
+                boolean neo4jIgnore = Boolean.valueOf(attrs.getValue("neo4j-ignore")).booleanValue();
+                if (neo4jIgnore) ignoredCollections.add(cls.name+"."+name);
                 
             }
         }
 
-        @Override
-        public void endElement(String uri, String localName, String qName) {
-            if ("class".equals(qName)) {
-                String[] parts = cls.name.split("\\.");
-                String className = parts[parts.length-1];
-                if (cls.neo4jIgnore) ignoredClasses.add(className);
-            }
-        }
-        
     }
 
     /**
@@ -245,30 +250,17 @@ public class Neo4jModelParser {
     static class SkeletonClass {
         String name;
         String supers;
-        boolean isInterface;
-        boolean neo4jIgnore;
-        Set<AttributeDescriptor> attributes = new LinkedHashSet<AttributeDescriptor>();
-        Set<ReferenceDescriptor> references = new LinkedHashSet<ReferenceDescriptor>();
-        Set<CollectionDescriptor> collections = new LinkedHashSet<CollectionDescriptor>();
 
         /**
          * Constructor.
          *
-         * @param packageName the name of the model package
          * @param name the fully qualified name of the described class
          * @param supers a space string of fully qualified class names
-         * @param isInterface true if describing an interface
          */
-        SkeletonClass(String packageName, String name, String supers, boolean isInterface, boolean neo4jIgnore) {
+        SkeletonClass(String name, String supers) {
             this.name = name;
-            if (this.name.startsWith(packageName + ".")) {
-                this.name = this.name.substring(packageName.length() + 1);
-            }
             if (this.name.contains(".")) {
-                throw new IllegalArgumentException("Class " + name + " is not in the model package " + packageName);
-            }
-            if (!"".equals(packageName)) {
-                this.name = packageName + "." + this.name;
+                throw new IllegalArgumentException("Class " + name + " is not in the model package.");
             }
             if (supers != null) {
                 String[] superNames = supers.split(" ");
@@ -276,15 +268,9 @@ public class Neo4jModelParser {
                 boolean needComma = false;
                 for (String superName : superNames) {
                     String origSuperName = superName;
-                    if (superName.startsWith(packageName + ".")) {
-                        superName = superName.substring(packageName.length() + 1);
-                    }
                     if (!"java.lang.Object".equals(superName)) {
                         if (superName.contains(".")) {
-                            throw new IllegalArgumentException("Superclass " + origSuperName + " of class " + this.name + " is not in the model package " + packageName);
-                        }
-                        if (!"".equals(packageName)) {
-                            superName = packageName + "." + superName;
+                            throw new IllegalArgumentException("Superclass " + origSuperName + " of class " + this.name + " is not in the model package.");
                         }
                     }
                     if (needComma) {
@@ -297,8 +283,6 @@ public class Neo4jModelParser {
             } else {
                 this.supers = null;
             }
-            this.isInterface = isInterface;
-            this.neo4jIgnore = neo4jIgnore;
         }
     }
 
