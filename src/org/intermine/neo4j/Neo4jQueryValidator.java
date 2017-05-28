1af90844f8d096e45c4ff1b0c91613f4ecf3e2e5
Yash Sharma <yasharmaster@gmail.com>
2017-05-28 16:18:44 +0530

    Add functionality to map connected nodes in schema generator

---
+++ src/org/intermine/neo4j/Neo4jQueryValidator.java
@@ -0,0 +1,9 @@
+package org.intermine.neo4j;
+
+/**
+ * Validates given cypher queries against the existing schema in the Neo4j database.
+ *
+ * @author Yash Sharma
+ */
+public class Neo4jQueryValidator {
+}
--- src/org/intermine/neo4j/Neo4jSchemaGenerator.java
+++ src/org/intermine/neo4j/Neo4jSchemaGenerator.java
@@ -23,7 +23,7 @@ public class Neo4jSchemaGenerator {
         mapConnectedNodes(driver);
 
         // Create Representative (Admin) nodes for all disconnected nodes
-        mapDisconnectedNodes(driver);
+//        mapDisconnectedNodes(driver);
     }
 
     private static void createInitialNodes(Driver driver){
@@ -42,10 +42,68 @@ public class Neo4jSchemaGenerator {
     }
 
     private static void mapConnectedNodes(Driver driver){
+        try (Session session = driver.session()) {
+            try (Transaction tx = session.beginTransaction()) {
+                // For all connected pair of nodes which are not in the metagraph,
+                // return the set of labels & properties of the nodes
+                // and the type & properties of their relationship.
+                String readQuery = "MATCH (n)-[r]->(m) " +
+                                    "WHERE NOT n:Metagraph AND NOT m:Metagraph " +
+                                    "RETURN DISTINCT labels(n), keys(n), " +
+                                    "type(r), keys(r), labels(m), keys(m)";
+                StatementResult result = tx.run(readQuery);
+                // For each node and relationship store NodeType and RelTpe nodes respectively,
+                // And add required relationships.
+                while (result.hasNext()) {
+                    Record record = result.next();
+                    Map<String, Object> params = getQueryParams(record);
+                    String writeQuery = getQuery();
+                    tx.run(writeQuery, params);
+                }
+                tx.success();
+                tx.close();
+                // Log the progress
+                System.out.println("Mapped Connected Nodes.");
+            }
+        }
+    }
 
+    private static String getQuery(){
+        String query = "MATCH (nodeOwner:Metagraph { metaType: 'NodeTypeOwner' }), " +
+                        "(relOwner:Metagraph { metaType: 'RelTypeOwner' }) " +
+                        "MERGE (nodeOwner)-[:OWNS]->(n:Metagraph { metaType: 'NodeType', " +
+                        "labels: '$startNodeLabels', properties: '$startNodeProperties'}) " +
+                        "MERGE (nodeOwner)-[:OWNS]->(m:Metagraph { metaType: 'NodeType', " +
+                        "labels: '$endNodeLabels', properties: '$endNodeProperties'}) " +
+                        "MERGE (relOwner)-[:OWNS]->(r:Metagraph { metaType: 'RelType', " +
+                        "type: '$relType', properties: '$relProperties'}) " +
+                        "MERGE (n)<-[:StartNodeType]-(r)-[:EndNodeType]->(m)";
+        return query;
+    }
+
+    private static HashMap<String, Object> getQueryParams(Record record){
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("relType", record.get("type(r)"));
+        params.put("relProperties", record.get("key(r)"));
+        params.put("startNodeLabels", record.get("labels(n)"));
+        params.put("startNodeProperties", record.get("key(n)"));
+        params.put("endNodeLabels", record.get("labels(m)"));
+        params.put("endNodeProperties", record.get("key(m)"));
+        return params;
     }
 
     private static void mapDisconnectedNodes(Driver driver){
-
+        try (Session session = driver.session()) {
+            try (Transaction tx = session.beginTransaction()) {
+                StatementResult result = tx.run("MATCH (n)-[r]-(m) " +
+                                                "RETURN DISTINCT labels(n),type(r),labels(m)");
+                while (result.hasNext()) {
+                    Record record = result.next();
+                    System.out.println(record.get("r").asList());
+                }
+                tx.success();
+                tx.close();
+            }
+        }
     }
 }
