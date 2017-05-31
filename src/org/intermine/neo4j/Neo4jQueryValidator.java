ca8a02f2f6f3eb66281c63e7c97705ea7b8abb2c
Yash Sharma <yasharmaster@gmail.com>
2017-05-31 07:56:39 +0530

    rename Neo4jQueryTester & remove Neo4jQueryValidator

--- src/org/intermine/neo4j/Neo4jQueryValidator.java
+++
@@ -1,9 +0,0 @@
-package org.intermine.neo4j;
-
-/**
- * Validates given cypher queries against the existing schema in the Neo4j database.
- *
- * @author Yash Sharma
- */
-public class Neo4jQueryValidator {
-}
