6dc9aaaa792edaa1e6756e6bf58b6ac131f19179
Yash Sharma <yasharmaster@gmail.com>
2017-05-31 20:10:12 +0530

    Add getModel method in Schema Generator

--- src/org/intermine/neo4j/Neo4jSchemaGenerator.java
+++ src/org/intermine/neo4j/Neo4jSchemaGenerator.java
@@ -242,4 +242,101 @@ public class Neo4jSchemaGenerator {
 
         return params;
     }
+
+    /**
+     * Generates a NodeDescriptor from a record.
+     * @param record a row of from neo4j result.
+     * @return A NodeDescriptor object based on the record.
+     */
+    private static NodeDescriptor getNodeDescriptor(Record record){
+        Set<String> labels = new HashSet<>(processList(record.get("labels").asList()));
+        Set<String> properties = new HashSet<>(processList(record.get("properties").asList()));
+
+        return new NodeDescriptor(labels, properties);
+    }
+
+    /**
+     * Generates a RelationshipDescriptor from a record.
+     * @param record a row of from neo4j result.
+     * @return A RelationshipDescriptor object based on the record.
+     */
+    private static RelationshipDescriptor getRelationshipDescriptor(Record record){
+        String type = record.get("relType").asString();
+        Value value = record.get("relProperties");
+        Set<String> properties;
+        if(!value.isNull() && !value.isEmpty()){
+            properties = new HashSet<>(processList(value.asList()));
+        }
+        else{
+            properties = new HashSet<>();
+        }
+        return new RelationshipDescriptor(type, properties);
+    }
+
+    /**
+     * Adds a set to the map of set of sets.
+     *
+     */
+    private static void addSetToMap(Map<String, Set<Set<String>>> map, String key, Set<String> set){
+        if(map.containsKey(key)){
+            Set<Set<String>> setOfSets = map.get(key);
+            setOfSets.add(set);
+            map.put(key, setOfSets);
+        }
+        Set<Set<String>> setOfSets = new HashSet<>();
+        setOfSets.add(set);
+        map.put(key, setOfSets);
+    }
+
+    /**
+     * Creates and returns a Model object from the metagraph
+     * @param driver Neo4j Java Driver instance.
+     * @return Model A model object which represents the metagraph (schema).
+     */
+    public static Model getModel(Driver driver){
+        Set<NodeDescriptor> nodes = new HashSet<>();
+        Set<RelationshipDescriptor> relationships = new HashSet<>();
+
+        Map<String, Set<Set<String>>> startNodes = new HashMap<>();
+        Map<String, Set<Set<String>>> endNodes = new HashMap<>();
+
+        try (Session session = driver.session()) {
+            try (Transaction tx = session.beginTransaction()) {
+                String query = "MATCH (n:Metagraph {metaType : 'NodeType'}) " +
+                        "RETURN DISTINCT n.labels as labels, n.properties as properties";
+                StatementResult result = tx.run(query);
+
+                // For each node create a NodeDescriptor
+                while (result.hasNext()) {
+                    Record record = result.next();
+                    nodes.add(getNodeDescriptor(record));
+                }
+
+                query = "MATCH (startNode:Metagraph)<-[:StartNodeType]-" +
+                        "(n:Metagraph {metaType : 'RelType'})-[:EndNodeType]->" +
+                        "(endNode:Metagraph) RETURN DISTINCT startNode.labels as startLabels, " +
+                        "endNode.labels as endLabels, n.type as relType, n.properties as relProperties";
+                result = tx.run(query);
+
+                // For relationship, create a RelationshipDescriptor and store start and end nodes
+                while (result.hasNext()) {
+                    Record record = result.next();
+                    relationships.add(getRelationshipDescriptor(record));
+                    String key = record.get("relType").asString();
+                    Set<String> startSet = new HashSet<>(processList(record.get("startLabels").asList()));
+                    Set<String> endSet = new HashSet<>(processList(record.get("endLabels").asList()));
+                    addSetToMap(startNodes, key, startSet);
+                    addSetToMap(endNodes, key, endSet);
+                }
+
+                tx.success();
+                tx.close();
+
+                // Log the progress
+                System.out.println("Model successfully extracted from the database.");
+            }
+        }
+        Model model = new Model(nodes, relationships, startNodes, endNodes);
+        return model;
+    }
 }
