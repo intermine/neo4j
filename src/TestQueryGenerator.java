41398df9a5a31832ea323e6a42b33ada9f8aa206
Yash Sharma <yasharmaster@gmail.com>
2017-07-05 17:45:47 +0530

    Add TemplateParser and Util

--- src/TestQueryGenerator.java
+++ src/TestQueryGenerator.java
@@ -1,6 +1,7 @@
 import org.intermine.neo4j.cypher.QueryGenerator;
 
-import java.io.*;
+import java.io.IOException;
+
 
 /**
  * Tests Query Generator.
@@ -11,15 +12,15 @@ public class TestQueryGenerator {
 
     public static void main(String args[]) throws IOException {
 
-        String FILENAME = "constraints/ISA";
+        String FILENAME = "constraints/WITHIN";
 
         String PATHQUERY_FILENAME = "tests/pathquery/" + FILENAME + ".xml";
-        String pathQuery = readFile(PATHQUERY_FILENAME);
+        String pathQuery = Util.readFile(PATHQUERY_FILENAME);
 
         String CYPHER_FILENAME = "tests/cypher/" + FILENAME+ ".cypher";
         String cypher = QueryGenerator.pathQueryToCypher(pathQuery);
 
-        writeFile(CYPHER_FILENAME, cypher);
+        Util.writeFile(CYPHER_FILENAME, cypher);
 
         printPathQueryCypher(pathQuery, cypher);
     }
@@ -31,37 +32,4 @@ public class TestQueryGenerator {
         System.out.println("-----------------------------------------");
     }
 
-    private static String readFile(String fileName) {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(fileName));
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-            while (line != null) {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
-            }
-            br.close();
-            return sb.toString();
-        } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Cannot read the file.");
-            System.exit(0);
-        }
-        return "";
-    }
-
-    private static void writeFile(String fileName, String content){
-        PrintWriter out = null;
-        try {
-            out = new PrintWriter(fileName);
-            out.println(content);
-            out.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Unable to write to file.");
-            System.exit(0);
-        }
-    }
 }
